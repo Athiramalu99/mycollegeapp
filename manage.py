d0f3614659619a8cba910a00d8fcff83f504eec5
Athiramalu99 <137681535+Athiramalu99@users.noreply.github.com>
2023-08-06 20:34:04 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schoolstore.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycollegewebsite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
